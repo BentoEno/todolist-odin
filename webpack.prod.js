6f1b7eac61bfb7ed597990067cfb8f4bea447d04
ahnafabid <aabid6000@gmail.com>
2026-05-19 16:57:42 +0900

    fix the most recent file

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,6 @@ import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
 
 export default merge (common, {
-    mode: 'Production',
+    mode: 'production',
     devtool: 'source-map',
 });
